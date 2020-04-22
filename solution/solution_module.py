87d9007299c9a076a417a7e65c0c8b143c09ec0a
Daniel Rodriguez <danielrs9705@gmail.com>
2020-04-22 22:24:33 +0200

    Implement the solution

--- solution/solution_module.py
+++ solution/solution_module.py
@@ -26,16 +26,70 @@ class SolutionModule:
         
         return visited_grid
 
+    def calculate_neighbors(self, row, column):
+        '''
+        Calculate the neighbors cells of a given position
+
+                _|_|_ -----> This is an example of a neighbor cell. The same for the rest. 
+                _|x|_
+                _|_|_
+
+        '''
+        neighbors_positions = [
+            (row, column - 1),
+            (row + 1, column),
+            (row, column + 1),
+            (row - 1, column),
+        ]
+        return neighbors_positions
+
+    def is_visited(self, visited_grid, position):
+        '''
+        Check if a cell is already visited.
+        '''
+        row = position[0]
+        column = position[1]
+        return visited_grid[row][column]
+
+    def is_valid_position(self, position, number_rows, number_columns):
+        '''
+        Check if the coordinates given are
+        valid
+        '''
+        row = position[0]
+        column = position[1]
+        return ((row >= 0 and row < number_rows) and (column >= 0 and column < number_columns))
+
     def dfs(self, position):
         '''
         Run the algorithm from the given position
         Position is a tuple (x, y) where x is the row position
         and y the column position
         '''
-        pass
+        row = position[0]
+        column = position[1]
+        self.visited_grid[row][column] = True
+        neighbors = self.calculate_neighbors(row, column)
+        for neighbor in neighbors:
+            if self.is_valid_position(neighbor, self.number_of_rows, self.number_of_columns):
+                if not self.is_visited(self.visited_grid, neighbor):
+                    self.dfs(neighbor)
 
-    def generate_answer(self):
+
+    def generate_answer(self, grid):
         '''
         Method that provides an answer to the problem
         '''
-        pass
+        self.visited_grid = self.generate_visited_grid(grid)
+        self.number_of_rows = len(self.visited_grid)
+        self.number_of_columns = len(self.visited_grid[0])
+
+        number_of_robots = 0
+        for idx_row, row in enumerate(self.visited_grid):
+            for idx_column, element in enumerate(row):
+                position = (idx_row, idx_column)
+                if not self.is_visited(self.visited_grid, position):
+                    number_of_robots += 1
+                    self.dfs(position)
+    
+        return number_of_robots
