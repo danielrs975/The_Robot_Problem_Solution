e9f20e806f28c778d797ea60f08939ba4f629873
Daniel Rodriguez <danielrs9705@gmail.com>
2020-04-22 20:41:20 +0200

    Add tests for the input module

---
+++ solution/input_test.py
@@ -0,0 +1,54 @@
+'''
+Suite of tests for the
+input module
+
+Author: Daniel Rodriguez
+'''
+import unittest
+
+from input_module import InputModule
+
+class TestInputModuleMethods(unittest.TestCase):
+    '''
+    Tests of the methods of the InputModule
+    '''
+    def setUp(self):
+        self.input_module = InputModule()
+
+    #----------------- is_valid method tests ----------------------------
+    def test_is_valid_with_valid_values(self):
+        cells = ['*', '*', '#', '*']
+        result = self.input_module.is_valid(cells)
+        self.assertEqual(result, True)
+
+    def test_is_valid_with_wrong_values(self):
+        cells = ['*', '1', 2, '#']
+        result = self.input_module.is_valid(cells)
+        self.assertEqual(result, False)
+    #---------------------------------------------------------------------
+    #------------------separate_data method tests ------------------------
+    def test_separate_data(self):
+        line = "* * # * * *"
+        expected_result = ['*', '*', '#', '*', '*', '*']
+        result = self.input_module.separate_data(line)
+        self.assertEqual(result, expected_result)
+    #---------------------------------------------------------------------
+    #----------------- generate_grid method tests ------------------------
+    def test_generate_grid_with_valid_grid(self):
+        string_grid = "* * #\n* # *\n* * *"
+        expected_result = [
+            ['*', '*', '#'],
+            ['*', '#', '*'],
+            ['*', '*', '*']
+        ]
+        result = self.input_module.generate_grid(string_grid)
+        self.assertEqual(result, expected_result)
+    
+    def test_generate_grid_with_invalid_grid(self):
+        string_grid = "* 1\n* *"
+        self.assertRaises(ValueError, self.input_module.generate_grid, string_grid)
+    #---------------------------------------------------------------------
+
+
+if __name__ == '__main__':
+    unittest.main()
